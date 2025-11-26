db4a36a1224d4d63d44718bf3d12f1d91e55da52
Manu <ecoraian@yahoo.com>
2025-11-26 19:30:06 +0200

    ex28 complet

---
+++ Coroian_Emanuel_Lab1M/Controllers/FirstController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coroian_Emanuel_Lab1M.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit în prima mea aplicatie ASP.NET Core MVC!";
+        }
+        public string Hello()
+        {
+            return "Salut, utilizatorule! Ma bucur ca ai ajuns aici.";
+        }
+
+        public string Mesaj(string nume, int varsta)
+        {
+            return $"Salut {nume}, ai {varsta} ani.";
+        }
+    }
+}
