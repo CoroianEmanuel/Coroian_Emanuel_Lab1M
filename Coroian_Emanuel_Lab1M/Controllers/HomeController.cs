3f38f535cd1eb7b34c7d8c8115c3627f73f3a63d
Manu <ecoraian@yahoo.com>
2025-11-26 19:16:56 +0200

    Add project files.

---
+++ Coroian_Emanuel_Lab1M/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Coroian_Emanuel_Lab1M.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coroian_Emanuel_Lab1M.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Coroian_Emanuel_Lab1M/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coroian_Emanuel_Lab1M.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
