e9a8411aebae788b45b5ae53d80fe8060105b15c
Manu <ecoraian@yahoo.com>
2025-11-26 19:43:47 +0200

    Ex1 complet

---
+++ Coroian_Emanuel_Lab1M/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Coroian_Emanuel_Lab1M.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coroian_Emanuel_Lab1M.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            var student = new Student { Name = "Ana", Age = 21 };
+            return View(student);
+        }
+    }
+}
---
+++ Coroian_Emanuel_Lab1M/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Coroian_Emanuel_Lab1M.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
