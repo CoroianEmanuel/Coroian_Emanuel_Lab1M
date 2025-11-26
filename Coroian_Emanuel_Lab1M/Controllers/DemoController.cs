48a1a36db469ccc666dbf27a7f4f897136f15ef9
Manu <ecoraian@yahoo.com>
2025-11-26 20:18:35 +0200

    Ex4 gata

---
+++ Coroian_Emanuel_Lab1M/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coroian_Emanuel_Lab1M.Controllers
+{
+    public class DemoController : Controller
+    {
+        [Route("hello")]
+        public string Hello() => "Salut!";
+
+        [HttpGet("hello/{name}")]
+        public string HelloByName(string name) => $"Salut, {name}!";
+
+        [HttpGet("calc/{a:int}/{b:int}")]
+        public string Calculate(int a, int b) => $"Suma este: {a + b}";
+    }
+}
